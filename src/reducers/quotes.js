daaecaf7765a64bab7841fc8d1f914134b0ea679
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-05-26 00:27:31 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,23 +1,38 @@
 export default (state = [], action) => {
-  console.log(action.type)
+  let index;
+  let quote;
+
   switch (action.type) {
 
     case 'ADD_QUOTE':
-      return [].concat(state, action.quote)
+      return state.concat(action.quote);
 
     case 'REMOVE_QUOTE':
-      return state.filter(quote => quote.id !== action.quoteId)
+      return state.filter(quote => quote.id !== action.quoteId);
 
     case 'UPVOTE_QUOTE':
-      return state.map(
-        quote => (quote.id === action.quoteId ? Object.assign({}, quote, {votes: quote.votes + 1}) : quote )
-        )
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+
+      return [
+        ...state.slice(0, index),
+        Object.assign({}, quote, { votes: quote.votes += 1 }),
+        ...state.slice(index + 1)
+      ];
 
     case 'DOWNVOTE_QUOTE':
-      return state.map(
-        quote => (quote.id === action.quoteId && quote.votes > 0 ? Object.assign({}, quote, {votes: quote.votes - 1}) : quote )
-        )
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+      if (quote.votes > 0) {
+        return [
+          ...state.slice(0, index),
+          Object.assign({}, quote, { votes: quote.votes -= 1 }),
+          ...state.slice(index + 1)
+        ];
+      }
+      return state;
 
-    default: return state
+    default: 
+      return state;
   }
 }
